56cdb6cb4f682a90dbb36c1afff78f7fce2bfc3d
dannyvoid <danielilluminatic@gmail.com>
2022-10-19 13:40:29 -0500

    fixes/qol
    auto installs a package if it isn't already.
    auto removes and reinstalls corrupted packages (packages which name in site-packages starts with ~),
    auto updates pip if possible. 
    
    still need to try and except if a package __can__ be installed, but cant be bothered

--- check_pip_upgrade.py
+++ check_pip_upgrade.py
@@ -1,47 +1,101 @@
 import subprocess
 import sys
+import os
+import shutil
+import pkg_resources
+import luddite
+import re
+
+auto_upgrade = ["pip"]
+
+
+def fix_corrupted_package():
+    site_packages = os.path.join(os.path.dirname(os.__file__), "site-packages")
+    site_packages = os.path.normpath(site_packages)
+
+    if os.path.exists(site_packages):
+        packages = os.listdir(site_packages)
+        packages = [package for package in packages if package.startswith("~")]
+
+    if packages:
+        for package in packages:
+            package = os.path.join(site_packages, package)
+            package = os.path.normpath(package)
+            shutil.rmtree(package)
+        return
+
+    else:
+        return
+
+
+def check_if_package_installed(package):
+    try:
+        pkg_resources.get_distribution(package)
+        return True
+    except pkg_resources.DistributionNotFound:
+        return False
+
+
+def install_package(package):
+    subprocess.call(f"python -m pip install {package}", shell=True)
 
 
 def check_local_package(package):
-    import re
     version = subprocess.check_output(f"python -m pip show {package}", shell=True)
-    version = version.decode('utf-8')
-    version = re.findall(r'\d+\.\d+\.\d+', version)
-    version = version[0]
+    version = version.decode("utf-8")
+    version = re.findall(r"Version: (.*)", version)[0]
     return version
 
 
 def check_remote_package(package):
-    import luddite
     version = luddite.get_version_pypi(package)
     return version
 
 
 def upgrade_package(package):
-    local_version = check_local_package(package)
-    remote_version = check_remote_package(package)
+    local_version = re.sub(r"[^\d.]", "", check_local_package(package))
+    remote_version = re.sub(r"[^\d.]", "", check_remote_package(package))
+
     if local_version == remote_version:
         print(f"{package} is up to date (local: {local_version})")
     else:
-        print(f"{package} is out of date (local: {local_version} < remote: {remote_version})")
+        print(
+            f"{package} is out of date (local: {local_version} < remote: {remote_version})"
+        )
         print(f"local: {local_version}")
         print(f"remote: {remote_version}")
 
-        if input("Do you want to upgrade? (y/n): ") == 'y':
+        if package in auto_upgrade:
+            print(f"Auto upgrading {package}")
             subprocess.call(f"python -m pip install --upgrade {package}", shell=True)
             print(f"{package} upgraded")
+
         else:
-            print(f"{package} not upgraded")
+            if input("Do you want to upgrade? (y/n): ") == "y":
+                subprocess.call(
+                    f"python -m pip install --upgrade {package}", shell=True
+                )
+                print(f"{package} upgraded")
+            else:
+                print(f"{package} not upgraded")
 
 
 def main():
     if len(sys.argv) > 1:
         package = sys.argv[1]
-        upgrade_package(package)
     else:
         package = input("Enter a package name: ")
-        upgrade_package(package)
-        input("Press enter to exit")
+
+    package = package.split()[0]
+
+    if package != "pip":
+        upgrade_package("pip")
+
+    if not check_if_package_installed(package):
+        fix_corrupted_package()
+        install_package(package)
+
+    upgrade_package(package)
 
 
 if __name__ == "__main__":
