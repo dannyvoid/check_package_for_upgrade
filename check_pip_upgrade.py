c8d840b46228308de4f6b328dc94f169afb947a1
dannyvoid <danielilluminatic@gmail.com>
2022-10-20 15:53:58 -0500

    Update check_pip_upgrade.py

--- check_pip_upgrade.py
+++ check_pip_upgrade.py
@@ -28,7 +28,7 @@ def fix_corrupted_package():
         return
 
 
-def check_if_package_installed(package):
+def package_installed(package):
     try:
         pkg_resources.get_distribution(package)
         return True
@@ -44,24 +44,27 @@ def install_package(package):
         print(f"{package} not installed")
 
 
-def check_local_package(package):
+def get_local_package_version(package):
     version = subprocess.check_output(f"python -m pip show {package}", shell=True)
     version = version.decode("utf-8")
     version = re.findall(r"Version: (.*)", version)[0]
+    version = re.sub(r"[^\d.]", "", version)
     return version
 
 
-def check_remote_package(package):
+def get_remote_package_version(package):
     version = luddite.get_version_pypi(package)
+    version = re.sub(r"[^\d.]", "", version)
     return version
 
 
 def upgrade_package(package):
-    local_version = re.sub(r"[^\d.]", "", check_local_package(package))
-    remote_version = re.sub(r"[^\d.]", "", check_remote_package(package))
+    local_version = get_local_package_version(package)
+    remote_version = get_remote_package_version(package)
 
     if local_version == remote_version:
         print(f"{package} is up to date (local: {local_version})")
+        return
     else:
         print(
             f"{package} is out of date (local: {local_version} < remote: {remote_version})"
@@ -73,7 +76,7 @@ def upgrade_package(package):
             print(f"Auto upgrading {package}")
             subprocess.call(f"python -m pip install --upgrade {package}", shell=True)
             print(f"{package} upgraded")
-
+            return
         else:
             if input("Do you want to upgrade? (y/n): ") == "y":
                 subprocess.call(
@@ -82,26 +85,27 @@ def upgrade_package(package):
                 print(f"{package} upgraded")
             else:
                 print(f"{package} not upgraded")
+            return
 
 
 def main():
     if len(sys.argv) > 1:
-        packages_to_check = sys.argv[1:]
+        package_list = sys.argv[1:]
     else:
-        packages_to_check = input(
+        package_list = input(
             "Enter a package name (seperate multiple package names by space): "
         )
 
-    if isinstance(packages_to_check, str):
-        packages_to_check = packages_to_check.split()
+    if isinstance(package_list, str):
+        package_list = package_list.split()
 
-    if "pip" not in packages_to_check:
-        packages_to_check.insert(0, "pip")
+    if "pip" not in package_list:
+        package_list.insert(0, "pip")
 
-    for package in packages_to_check:
+    for package in package_list:
         package = re.sub(r"[^\w.-]", "", package)
 
-        if check_if_package_installed(package):
+        if package_installed(package):
             upgrade_package(package)
         else:
             print(f"{package} is not installed")
