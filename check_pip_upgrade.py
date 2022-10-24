1c518b8b392442bbc0d4ac7533ae3bb615781d14
dannyvoid <danielilluminatic@gmail.com>
2022-10-24 02:52:02 -0500

    Update check_pip_upgrade.py
    not automatically updating/installing pip anymore

--- check_pip_upgrade.py
+++ check_pip_upgrade.py
@@ -1,25 +1,15 @@
-import os, sys, shutil
-import pip, pkg_resources
+import os, sys, shutil, pip
 import luddite
+from pkg_resources import parse_version, get_distribution, DistributionNotFound
 
-auto_upgrade = ["pip"]
-
-
-def fix_corrupted_package():
-    site_packages = os.path.join(os.path.dirname(os.__file__), "site-packages")
-    site_packages = os.path.normpath(site_packages)
-
-    if os.path.exists(site_packages):
-        for package in os.listdir(site_packages):
-            if package.startswith("~"):
-                shutil.rmtree(os.path.join(site_packages, package))
+auto_upgrade = []
 
 
 def package_installed(package):
     try:
-        pkg_resources.get_distribution(package)
+        get_distribution(package)
         return True
-    except pkg_resources.DistributionNotFound:
+    except DistributionNotFound:
         return False
 
 
@@ -31,24 +21,23 @@ def install_package(package):
         print(f"{package} not installed")
 
 
-def get_local_package_version(package):
-    return pkg_resources.get_distribution(package).version
-
-
-def get_remote_package_version(package):
-    return luddite.get_version_pypi(package)
-
-
 def upgrade_package(package):
-    local_version = get_local_package_version(package)
-    remote_version = get_remote_package_version(package)
+    local_version = get_distribution(package).version
+    remote_version = luddite.get_version_pypi(package)
 
     if local_version == remote_version:
         print(f"{package} is up to date (local: {local_version})")
     else:
-        print(
-            f"{package} is out of date (local: {local_version} < remote: {remote_version})"
-        )
+        if parse_version(local_version) < parse_version(remote_version):
+            print(
+                f"{package} is out of date (local: {local_version} < remote: {remote_version})"
+            )
+
+        if parse_version(local_version) > parse_version(remote_version):
+            print(
+                f"{package} is newer than remote (local: {local_version} > remote: {remote_version})"
+            )
+
         print(f"local: {local_version}")
         print(f"remote: {remote_version}")
 
@@ -64,6 +53,16 @@ def upgrade_package(package):
                 print(f"{package} not upgraded")
 
 
+def fix_corrupted_package():
+    site_packages = os.path.join(os.path.dirname(os.__file__), "site-packages")
+    site_packages = os.path.normpath(site_packages)
+
+    if os.path.exists(site_packages):
+        for package in os.listdir(site_packages):
+            if package.startswith("~"):
+                shutil.rmtree(os.path.join(site_packages, package))
+
+
 def main():
     if len(sys.argv) > 1:
         package_list = sys.argv[1:]
@@ -74,9 +73,6 @@ def main():
 
     if isinstance(package_list, str):
         package_list = package_list.split()
-
-    if "pip" not in package_list:
-        package_list.insert(0, "pip")
 
     for package in package_list:
         if package_installed(package):
